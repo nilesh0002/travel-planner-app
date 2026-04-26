a8f30d1b21b1ab47399ede1130ff40ad06686753
nilesh0002 <nilesh.singh2024@lpu.in>
2026-04-26 19:26:10 +0530

    do

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-https://traveling-planner.vercel.app/loginimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 // Pages
